9e411f5181a479834c3290e36e22ed6f58308ffa
swapnil shinde <swapnilshinde9382@gmail.com>
2022-01-22 06:28:26 -0800

    init commit

---
+++ config.py
@@ -0,0 +1,52 @@
+
+from socketserver import DatagramRequestHandler
+
+
+sleep_in_between = 5
+
+message = {
+    "message,1": "Good evening, <name>",
+
+    "message,2": """
+TEPAT IS OFFERING YOU A *FREE CANVA (GRAPHIC DESIGN)* TRAINING with limited seats available❗❗❗
+& stand a chance to WIN CASH PRIZE for logo design
+    
+We give priority to those who joined Digital Marketing training claimed under EIS PERKESO.  
+    """,
+
+    "media,1": "event photo.jpeg",
+
+    "message,3": """
+*Training Details*
+✅ The classes will be conducted bilingually (BM + English).
+✅ The classes will be 100% online, conducted on the Microsoft Teams platform.
+✅ The participants will learn A to Z about Canva in 8 hours training.
+✅ Classes will be conducted only on Saturdays (4 hours per day).
+✅ The training dates are 12th February 2022 and 19th February 2022 from 9:00am until 1:00pm.
+    """,
+
+    "message,4": """
+*Learning outcome*
+❤️Design a professional-looking poster within 15 minutes.
+❤️Participate in Tepat’s Logo Designing Competition, and stand a chance to win a cash prize.
+    """,
+
+    "media,2": "Tepat Free Canva Training Brochure.pdf",
+
+    "message,5": """
+Click the link below to register your FREE SEAT !!!!
+
+https://www.tepat.net/canva-free-training/
+   
+    """,
+    
+    }
+
+url = "https://docs.google.com/spreadsheets/d/16cfEsfUVomuTAPlRLVvpX4YAiJx8ocPGa_yiOR6Lqls/edit?usp=sharing"
+
+a = 1
+a = 3
+
+a = {
+    "message,133"
+}
